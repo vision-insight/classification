4048f4d1f2c6b3ace5850f43b0710713fc474f65
nannan-sys <1296482672@qq.com>
2020-03-12 15:09:54 +0800

    update the data_split.py

---
+++ gender/data_prepare.py
@@ -0,0 +1,126 @@
+import os
+import sys
+import torch
+import pathlib
+from torch.utils.data import Dataset, DataLoader
+from torchvision import datasets, models, transforms
+import PIL
+from PIL import Image
+from multiprocessing import cpu_count
+
+base_path = "/data/lulei/classification"
+sys.path.insert(0, base_path)
+from tools.utils.torch_utils import *
+from tools.utils.utils import *
+from torchvision.datasets import ImageFolder
+os.system("clear")
+
+
+################## 00 variable  Assignment ################################
+
+# Height and width of the CNN input image
+img_h, img_w = 227, 227
+
+# Set train and valid directory paths
+dataset_dir = "/data/lulei/data/vehicle/frontal_103/split"
+
+# Batch size
+batch_size = 128
+print("[INFO] batch size : ", batch_size)
+
+
+train_data_dir= os.path.join(dataset_dir, 'train')
+valid_data_dir = os.path.join(dataset_dir, 'valid')
+test_data_dir = os.path.join(dataset_dir, 'test')
+
+
+########## 001 Data Transforms #####################
+
+image_transforms = { 
+
+            # transforms (a.k.a data augmentations) for the training images
+            'train': transforms.Compose([
+                # transfer the input image into gray scale
+                #transforms.Grayscale(num_output_channels=1),
+                # resize the input image into the predefined scale
+                transforms.Resize((img_h, img_w), interpolation=PIL.Image.BICUBIC), # (h, w)
+                # random choose one of the predefined transforms (in the list) when performing the training process
+                #transforms.RandomChoice([transforms.RandomHorizontalFlip(),
+                #                        transforms.ColorJitter(brightness=0.3, contrast=0.3, saturation=0.3, hue=0.3)]),
+                #transforms.Lambda(lambda img : pad_img(img, img_w)),
+                # transfer the type of input image into tensor style
+                transforms.ToTensor(),
+                                        ]),
+
+            # transforms for the valid images
+            'valid': transforms.Compose([
+                #transforms.Grayscale(num_output_channels=1),
+                transforms.Resize((img_h, img_w), interpolation=PIL.Image.BICUBIC),
+                #transforms.Lambda(lambda img : pad_img(img, img_w)),
+                transforms.ToTensor(),
+                                        ]),
+
+            # transforms for the test images
+            'test': transforms.Compose([
+                #transforms.Grayscale(num_output_channels=1),
+                transforms.Resize((img_h, img_w), interpolation=PIL.Image.BICUBIC),
+                #transforms.Lambda(lambda img : pad_img(img, img_w)),
+                transforms.ToTensor(),
+                                       ])
+                    }
+
+
+############## 002 Load Data from folders   ##################
+
+data = {
+        'train': ImageFolder(root=train_data_dir,
+                             transform=image_transforms['train'], ),
+
+        'valid': ImageFolder(root=valid_data_dir, 
+                             transform=image_transforms['valid'],
+                             target_transform=None),
+
+
+        'test': ImageFolder(root=test_data_dir, 
+                            transform=image_transforms['test'],
+                            target_transform=None)
+        }
+
+
+############# 003 Data iterators (Data loader) ###########################
+
+dataloaders = {
+        "train": DataLoader(data['train'], 
+                            batch_size=batch_size, 
+                            shuffle=True,
+                            num_workers= cpu_count()//2),
+
+        "valid": DataLoader(data['valid'], 
+                            batch_size=batch_size*2, 
+                            shuffle=True,
+                            num_workers= cpu_count()//2),
+
+        "test":  DataLoader(data['test'],
+                            batch_size=batch_size*2, 
+                            shuffle=True,
+                            num_workers= cpu_count()//2)
+            }
+
+
+############ 004 get the weights of each classes ############
+
+class_to_index = data["train"].class_to_idx
+if len(class_to_index) <= 20: print("[INFO] class to index : ",class_to_index)
+
+class_weights = get_class_weights(train_data_dir, class_to_index, idx_first = False)*10
+print("[INFO] class weights : ", class_weights)
+
+############# 005 show the image quantity in each set ##########
+for data_type in ["train", "valid", "test"]:
+    temp_1 = len(data[data_type])
+    print("[INFO] image for %s : %d" % (data_type, temp_1))
+        
+
+
+if __name__ == "__main__":
+    pass
--- tools/data_split.py
+++ tools/data_split.py
@@ -3,7 +3,99 @@ import pathlib
 import shutil
 import random
 from PIL import Image
+from tqdm import tqdm
 
+
+class data_split:
+    def __init__(self, config_file):
+        self.cfg_file = config_file
+    
+    def config_reader(self):
+         with open(self.cfg_file,encoding='UTF-8') as f:
+             content = f.readlines() #encoding='UTF-8'
+         for line in content:
+             temp = [i for i in line.replace('\t','  ').split("#")[0].strip().split(' ') \
+                     if    i != '']
+             if temp == []:
+                 continue
+
+             if "source_dir" in temp:
+                 self.source_dir = temp[1] #self.ip_pattern.findall(temp)[0][0]
+                 print(f"[CONFIG INFO] source folder : {self.source_dir}")
+                 continue
+
+             elif "dest_dir" in temp:
+                 self.dest_dir = temp[1]
+                 print(f"[CONFIG INFO] dest folder: {self.dest_dir}")
+                 continue
+
+             elif "train_ratio" in temp:
+                 self.train_ratio = float(temp[1])
+                 print(f"[CONFIG INFO] train ratio : {self.train_ratio}")
+                 continue
+
+             elif "valid_ratio" in temp:
+                 self.valid_ratio = float(temp[1])
+                 print(f"[CONFIG INFO] valid ratio : {self.valid_ratio}")
+                 continue
+
+             elif "test_ratio" in temp:
+                 self.test_ratio = float(temp[1]) 
+                 print(f"[CONFIG INFO] test ratio : {self.test_ratio}")
+                 continue
+
+             elif "shuffle" in temp:
+                 self.shuffle = True if temp[1] == "True" else False
+                 print(f"[CONFIG INFO] shuffle? : {self.shuffle}")
+                 continue
+
+    def split(self):
+        self.config_reader()
+        assert self.train_ratio + self.valid_ratio + self.test_ratio == 1,\
+                print('[INFO] sum of all ratio should be 1')
+
+        if self.train_ratio != 0:
+            train_dir = os.path.join(self.dest_dir, 'train')
+            mkdir(train_dir)
+    
+        if self.valid_ratio != 0:
+            val_dir = os.path.join(self.dest_dir, 'valid')
+            mkdir(val_dir)
+    
+        if self.test_ratio != 0:
+            test_dir = os.path.join(self.dest_dir, 'test')
+            mkdir(test_dir)
+    
+        for class_name in tqdm(os.listdir(self.source_dir)):
+            class_dir = os.path.join(self.source_dir, class_name)
+            if not os.path.isdir(class_dir):
+                continue
+            image_paths = [str(i) for i in pathlib.Path(class_dir).rglob("*.jpg")]
+    
+            train_images, val_images, test_images = \
+                            list_split(image_paths, ratio = [self.train_ratio, self.valid_ratio, self.test_ratio])
+    
+            if self.train_ratio != 0 and os.path.isdir(train_dir):
+                dest_class_dir = os.path.join(train_dir, class_name)
+                mkdir(dest_class_dir)
+                [shutil.copy(i, dest_class_dir) for i in train_images]
+    
+            if self.valid_ratio != 0 and os.path.isdir(valid_dir):
+                dest_class_dir = os.path.join(val_dir, class_name)
+                mkdir(dest_class_dir)
+                [shutil.copy(i, dest_class_dir) for i in val_images]
+    
+            if self.test_ratio != 0 and os.path.isdir(test_dir):
+                dest_class_dir = os.path.join(test_dir, class_name)
+                mkdir(dest_class_dir)
+                [shutil.copy(i, dest_class_dir) for i in test_images]
+
+
+
+def mkdir(*path):
+    for p in path:
+        if not os.path.exists(p):
+            os.makedirs(p)
 
 def list_split(input_list, ratio = [0.5, 0.5], shuffle = False):
     assert sum(ratio) == 1, print('sum of ratio must equals to 1')
@@ -19,71 +111,10 @@ def list_split(input_list, ratio = [0.5, 0.5], shuffle = False):
     return new_list
 
 
-def data_split(image_root_folder, dest_folder = './', train_ratio = 0, val_ratio = 0, test_ratio = 0, shuffle = True):
-
-    assert train_ratio + val_ratio + test_ratio == 1, print('[INFO] sum of all ratio should be 1')
-
-    if train_ratio != 0:
-        train_dir = os.path.join(dest_folder, 'train')
-        mkdir(train_dir)
-
-    if val_ratio != 0:
-        val_dir = os.path.join(dest_folder, 'valid')
-        mkdir(val_dir)
-
-    if test_ratio != 0:
-        test_dir = os.path.join(dest_folder, 'test')
-        mkdir(test_dir)
-
-    for class_name in os.listdir(image_root_folder):
-        class_dir = os.path.join(image_root_folder, class_name)
-        if not os.path.isdir(class_dir):
-            continue
-        image_paths = [ os.path.join(class_dir, file_path)
-                       for file_path in os.listdir(class_dir)
-                       if file_path[-3:] == 'jpg']
-
-        train_images, val_images, test_images = \
-                        list_split(image_paths, ratio = [train_ratio, val_ratio, test_ratio])
-
-        if os.path.isdir(train_dir):
-            dest_class_dir = os.path.join(train_dir, class_name)
-            mkdir(dest_class_dir)
-            [shutil.copy(i, dest_class_dir) for i in train_images]
-
-        if os.path.isdir(val_dir):
-            dest_class_dir = os.path.join(val_dir, class_name)
-            mkdir(dest_class_dir)
-            [shutil.copy(i, dest_class_dir) for i in val_images]
-
-        if test_ratio != 0:
-            dest_class_dir = os.path.join(test_dir, class_name)
-            mkdir(dest_class_dir)
-            [shutil.copy(i, dest_class_dir) for i in test_images]
-
-
-
-def mkdir(*path):
-    for p in path:
-        if not os.path.exists(p):
-            os.makedirs(p)
-
-
 if __name__ == "__main__":
 
-
-    train_ratio = 0.8
-    val_ratio = 0.2
-    test_ratio = 0
-
-    image_root_folder = "/data/lulei/data/age/version_1/big_pic"
-    dest_folder = "/data/lulei/data/age/version_1/data_split"
-
-    data_split(image_root_folder, 
-               dest_folder, 
-               train_ratio, 
-               val_ratio, 
-               test_ratio, 
-               shuffle = True )
-
+    config_file = "/media/D/lulei/classification/tools/data_split_config.cfg"
+    
+    ds = data_split(config_file = config_file)
+    ds.split()
 
