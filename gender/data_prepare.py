89e26d1b0a6887c239d860865c04b2163261ee35
nannan-sys <1296482672@qq.com>
2020-03-12 17:42:39 +0800

    update

--- gender/data_prepare.py
+++ gender/data_prepare.py
@@ -35,75 +35,69 @@ test_data_dir = os.path.join(dataset_dir, 'test')
 
 ########## 001 Data Transforms #####################
 
-image_transforms = { 
+image_trans = { 
+    'train': transforms.Compose([
+        # transfer the input image into gray scale
+        #transforms.Grayscale(num_output_channels=1),
+        # resize the input image into the predefined scale
+        transforms.Resize((img_h, img_w), interpolation=PIL.Image.BICUBIC), # (h, w)
+        # random choose one of the predefined transforms (in the list) when performing the training process
+        # transforms.RandomChoice([transforms.RandomHorizontalFlip(),
+        # transforms.ColorJitter(brightness=0.3, contrast=0.3, saturation=0.3, hue=0.3)]),
+        #transforms.Lambda(lambda img : pad_img(img, img_w)),
+        # transfer the type of input image into tensor style
+        transforms.ToTensor(),
+                                ]),
 
-            # transforms (a.k.a data augmentations) for the training images
-            'train': transforms.Compose([
-                # transfer the input image into gray scale
-                #transforms.Grayscale(num_output_channels=1),
-                # resize the input image into the predefined scale
-                transforms.Resize((img_h, img_w), interpolation=PIL.Image.BICUBIC), # (h, w)
-                # random choose one of the predefined transforms (in the list) when performing the training process
-                #transforms.RandomChoice([transforms.RandomHorizontalFlip(),
-                #                        transforms.ColorJitter(brightness=0.3, contrast=0.3, saturation=0.3, hue=0.3)]),
-                #transforms.Lambda(lambda img : pad_img(img, img_w)),
-                # transfer the type of input image into tensor style
-                transforms.ToTensor(),
-                                        ]),
+    'valid': transforms.Compose([
+        #transforms.Grayscale(num_output_channels=1),
+        transforms.Resize((img_h, img_w), interpolation=PIL.Image.BICUBIC),
+        #transforms.Lambda(lambda img : pad_img(img, img_w)),
+        transforms.ToTensor(),
+                                ]),
 
-            # transforms for the valid images
-            'valid': transforms.Compose([
-                #transforms.Grayscale(num_output_channels=1),
-                transforms.Resize((img_h, img_w), interpolation=PIL.Image.BICUBIC),
-                #transforms.Lambda(lambda img : pad_img(img, img_w)),
-                transforms.ToTensor(),
-                                        ]),
-
-            # transforms for the test images
-            'test': transforms.Compose([
-                #transforms.Grayscale(num_output_channels=1),
-                transforms.Resize((img_h, img_w), interpolation=PIL.Image.BICUBIC),
-                #transforms.Lambda(lambda img : pad_img(img, img_w)),
-                transforms.ToTensor(),
-                                       ])
-                    }
-
+    'test': transforms.Compose([
+        #transforms.Grayscale(num_output_channels=1),
+        transforms.Resize((img_h, img_w), interpolation=PIL.Image.BICUBIC),
+        #transforms.Lambda(lambda img : pad_img(img, img_w)),
+        transforms.ToTensor(),
+                               ])
+             }
 
 ############## 002 Load Data from folders   ##################
 
 data = {
-        'train': ImageFolder(root=train_data_dir,
-                             transform=image_transforms['train'], ),
+    'train': ImageFolder(root=train_data_dir,
+                         transform=image_trans['train'], ),
 
-        'valid': ImageFolder(root=valid_data_dir, 
-                             transform=image_transforms['valid'],
-                             target_transform=None),
+    'valid': ImageFolder(root=valid_data_dir, 
+                         transform=image_trans['valid'],
+                         target_transform=None),
 
-        'test': ImageFolder(root=test_data_dir, 
-                            transform=image_transforms['test'],
-                            target_transform=None)
+    'test': ImageFolder(root=test_data_dir, 
+                        transform=image_trans['test'],
+                        target_transform=None)
         }
 
 
 ############# 003 Data iterators (Data loader) ###########################
 
 dataloaders = {
-        "train": DataLoader(data['train'], 
-                            batch_size=batch_size, 
-                            shuffle=True,
-                            num_workers= cpu_count()//2),
+    "train": DataLoader(data['train'], 
+                        batch_size=batch_size, 
+                        shuffle=True,
+                        num_workers= cpu_count()//2),
 
-        "valid": DataLoader(data['valid'], 
-                            batch_size=batch_size*2, 
-                            shuffle=True,
-                            num_workers= cpu_count()//2),
+    "valid": DataLoader(data['valid'], 
+                        batch_size=batch_size*2, 
+                        shuffle=True,
+                        num_workers= cpu_count()//2),
 
-        "test":  DataLoader(data['test'],
-                            batch_size=batch_size*2, 
-                            shuffle=True,
-                            num_workers= cpu_count()//2)
-            }
-
+    "test":  DataLoader(data['test'],
+                        batch_size=batch_size*2, 
+                        shuffle=True,
+                        num_workers= cpu_count()//2)
+              }
 
 ############ 004 get the weights of each classes ############
 
@@ -115,8 +109,7 @@ print("[INFO] class weights : ", class_weights)
 
 ############# 005 show the image quantity in each set ##########
 for data_type in ["train", "valid", "test"]:
-    temp_1 = len(data[data_type])
-    print("[INFO] image for %s : %d" % (data_type, temp_1))
+    print(f"[INFO] image for {data_type} : {len(data[data_type])}")
         
 
 if __name__ == "__main__":
