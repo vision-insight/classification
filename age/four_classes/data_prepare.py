118591e199067d6c8c7076f0ccff19d755d667af
vola <lustory@163.com>
2020-03-23 15:44:14 +0800

    update

--- age/four_classes/data_prepare.py
+++ age/four_classes/data_prepare.py
@@ -2,13 +2,13 @@ import os
 import sys
 import torch
 import pathlib
-from torch.utils.data import Dataset, DataLoader
+from torch.utils.data import Dataset, DataLoader, random_split
 from torchvision import datasets, models, transforms
 import PIL
 from PIL import Image
 from multiprocessing import cpu_count
 
-base_path = "/data/lulei/classification"
+base_path = "/media/D/lulei/classification"
 sys.path.insert(0, base_path)
 from tools.utils.torch_utils import *
 from tools.utils.utils import *
@@ -22,13 +22,13 @@ os.system("clear")
 img_h, img_w = 180,180
 
 # Set train and valid directory paths
-dataset_dir = "/data/lulei/data/age/version_1/data_split"
+dataset_dir = "/media/D/lulei/data/age/split"
 
 # Set the index and the corresponding class name (folder name) of the classes
-idx_and_class = {0 : ['0-6', '6-12'],
-                 1 : ['12-18', '18-25'],
-                 2 : ['25-40', '40-55'],
-                 3 : ['55+']}
+idx_and_class = {0 : ['0-8', '8-18'],
+                 1 : ['18-25', '25-35'],
+                 2 : ['35-45', '45-65'],
+                 3 : ['65+']}
 # Batch size
 batch_size = 256
 print("[INFO] batch size : ", batch_size)
@@ -44,24 +44,24 @@ test_data_dir = os.path.join(dataset_dir, 'test')
 image_transforms = { 
     # transforms (a.k.a data augmentations) for the training images
     'train': transforms.Compose([
-                                 # transfer the input image into gray scale
-                                 #transforms.Grayscale(num_output_channels=1),
-                                 # resize the input image into the predefined scale
-                                 #transforms.Resize((img_h, img_w), interpolation=PIL.Image.BICUBIC), # (h, w)
-                                 # random choose one of the predefined transforms (in the list) when performing the training process
-                                 #transforms.RandomChoice([transforms.RandomHorizontalFlip(),
-                                 #transforms.ColorJitter(brightness=0.3, contrast=0.3, saturation=0.3, hue=0.3)]),
-                                 transforms.Lambda(lambda img : pad_img(img, img_w)),
-                                 # transfer the type of input image into tensor style
-                                 transforms.ToTensor(),
+        # transfer the input image into gray scale
+        #transforms.Grayscale(num_output_channels=1),
+        # resize the input image into the predefined scale
+        #transforms.Resize((img_h, img_w), interpolation=PIL.Image.BICUBIC), # (h, w)
+        # random choose one of the predefined transforms (in the list) when performing the training process
+        #transforms.RandomChoice([transforms.RandomHorizontalFlip(),
+        #transforms.ColorJitter(brightness=0.3, contrast=0.3, saturation=0.3, hue=0.3)]),
+        transforms.Lambda(lambda img : pad_img(img, img_w)),
+        # transfer the type of input image into tensor style
+        transforms.ToTensor(),
                                 ]),
 
     # transforms for the valid images
     'valid': transforms.Compose([
-                                 #transforms.Grayscale(num_output_channels=1),
-                                 #transforms.Resize((img_h, img_w), interpolation=PIL.Image.BICUBIC),
-                                 transforms.Lambda(lambda img : pad_img(img, img_w)),
-                                 transforms.ToTensor(),
+        #transforms.Grayscale(num_output_channels=1),
+        #transforms.Resize((img_h, img_w), interpolation=PIL.Image.BICUBIC),
+        transforms.Lambda(lambda img : pad_img(img, img_w)),
+        transforms.ToTensor(),
                                 ]),
                   }
 
@@ -80,17 +80,23 @@ data = {
 
 
 ############# 003 Data iterators (Data loader) ###########################
+train_set, valid_set = random_split(data['train'], (100, len(data['train']) -100))
+
+
+
+
+
 
 dataloaders = {
-               "train": DataLoader(data['train'], 
-                                   batch_size=batch_size, 
-                                   shuffle=True,
-                                   num_workers= cpu_count()//2),
+        "train": DataLoader(data['train'], 
+                            batch_size=batch_size, 
+                            shuffle=True,
+                            num_workers= cpu_count()//2),
 
-               "valid": DataLoader(data['valid'], 
-                                   batch_size=batch_size, 
-                                   shuffle=True,
-                                   num_workers= cpu_count()//2),
+        "valid": DataLoader(data['valid'], 
+                            batch_size=batch_size, 
+                            shuffle=True,
+                            num_workers= cpu_count()//2),
               }
 
 
@@ -104,10 +110,5 @@ print("[INFO] class weights : ", class_weights)
 
 ############# 005 show the image quantity in each set ##########
 for data_type in ["train", "valid"]:
-    temp = len(data[data_type])
-    print("[INFO] image for %s : %d" % (data_type, temp))
+    print(f"[INFO] image for {data_type} : {len(data[data_type])}")
         
-
-
-if __name__ == "__main__":
-    pass
