0458b7db053e9d4d500a93f37d48f985b4cc5a30
vola <lustory@163.com>
2020-05-19 15:30:52 +0800

    xjtu update

--- age/four_classes/.ipynb_checkpoints/data_prepare-checkpoint.py
+++ age/four_classes/.ipynb_checkpoints/data_prepare-checkpoint.py
@@ -6,11 +6,8 @@ from multiprocessing import cpu_count
 
 base_path = "/media/D/lulei/classification"
 sys.path.insert(0, base_path)
-<<<<<<< HEAD
 from tools.utils.torch_utils import *
-=======
 from classification.tools.utils.torch_utils import *
->>>>>>> ad5c491cd25727ada442831ef5986d4222cb7c8a
 from tools.utils.utils import *
 from tools.utils.sampler import BalancedBatchSampler
 from tools.utils.ImageFolder import ImageFolder
@@ -97,7 +94,6 @@ dataloaders = {
 
                 "valid": DataLoader(data["valid"],
                                     batch_size=batch_size,
->>>>>>> 327dc0d1483608f664649dbfd34c0a79fc981db1
                                     shuffle=True,
                                     num_workers= cpu_count()),
                               }
